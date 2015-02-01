8d434d6bce0c694268fff65af516b9a4aa38cf9c
tgdobreva <tgdobreva@gmail.com>
2015-02-01 21:01:59 +0200

    bootstrap theme added

--- HirundoWebApp/src/main/java/com/teodoradobreva/mongodb/rest/LoginRest.java
+++ HirundoWebApp/src/main/java/com/teodoradobreva/mongodb/rest/LoginRest.java
@@ -4,12 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/login")
-public class HelloWorldREST {
+public class LoginRest {
 
 	@GET
 	@Path("/hello")
 	public String sayHello() {
 		return "Hello asd";
+	}
+	
+	@GET
+	@Path("/kur")
+	public String kur()
+	{
+		return "kur";
 	}
 
 }
